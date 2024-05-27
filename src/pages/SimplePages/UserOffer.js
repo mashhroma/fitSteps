ede23d48493b39b483257e349cf7b998e220a99c
mashhroma <mashh@mail.ru>
2024-05-27 13:49:39 +0300

    added page User Offer

--- src/pages/SimplePages/UserOffer.js
+++ src/pages/SimplePages/UserOffer.js
@@ -3,7 +3,7 @@ import React from "react";
 export function UserOffer() {
     return (
         <section className="text">
-            
+            <p>В данном разделе будет опубликована Оферта с пользователем.</p>
         </section>
     )
 }
