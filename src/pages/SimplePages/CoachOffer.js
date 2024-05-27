3e83df3923c50dd56969b8ae7f80cae34a16e3c3
mashhroma <mashh@mail.ru>
2024-05-27 13:49:04 +0300

    added page Coach Offer

--- src/pages/SimplePages/CoachOffer.js
+++ src/pages/SimplePages/CoachOffer.js
@@ -3,7 +3,7 @@ import React from "react";
 export function CoachOffer() {
     return (
         <section className="text">
-            
+            <p>В данном разделе будет опубликована Оферта с тренером.</p>
         </section>
     )
 }
