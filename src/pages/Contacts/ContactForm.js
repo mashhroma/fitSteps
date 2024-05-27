230e99e57b81fe78693d42b7793486c43c5ae221
mashhroma <mashh@mail.ru>
2024-05-27 15:33:20 +0300

    added page Contacts

--- src/pages/Contacts/ContactForm.js
+++ src/pages/Contacts/ContactForm.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export function ContactForm() {
     return (
-        <form>
-            <input type='text' required placeholder='Ваше имя' />
-            <input type='email' required placeholder='Ваша эл.почта' />
-            <input type='number' placeholder='Ваш телефон' />
-            <textarea placeholder='Введите Ваше сообщение'></textarea>
-            <button>Отправить</button>
+        <form className='contact'>
+            <input className='contact__input' type='text' required placeholder='Ваше имя' />
+            <input className='contact__input' type='email' required placeholder='Ваша эл.почта' />
+            <input className='contact__input' type='number' placeholder='Ваш телефон' />
+            <textarea className='contact__input' placeholder='Введите Ваше сообщение'></textarea>
+            <button className='contact__button'>Отправить</button>
         </form>
     )
 }
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -3,7 +3,8 @@ import { ContactForm } from './ContactForm';
 
 export function Contacts() {
     return (
-        <section>
+        <section className='text'>
+            <h3>Уважаемые посетители сайта!</h3>
             <p>По любым вопросам Вы всегда можете обратиться к нам по телефону, электронной почте, либо оставить сообщение, заполнив форму ниже.</p>
             <p>+7 800 000-00-00</p>
             <p>info@fitsteps.ru</p>
